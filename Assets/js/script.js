9a4ba5e5179ecb500c6b74aade8f26ea574c4743
Kyle <kylejmbaylis@gmail.com>
2022-03-24 16:58:33 -0400

    styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,9 @@
+// current date for header
 var date = String(moment().format("dddd, MMMM Do"));
 
+// for each time block to compare to for coloring
+var currentHour = String(moment().format("HH"));
+
+// to add current date to header
 $("<p>" + date + "</p>").appendTo("#currentDay");
+
